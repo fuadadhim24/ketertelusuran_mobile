33cfbb90b3202f212dc1fd614c1ecdac37db0af2
rizkypradika <rizkypradika27@gmail.com>
2024-03-28 02:09:42 +0700

    feat: add snackbar notifications sucess & failed at sign in page

--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:ketertelusuran_mobile/shared/theme.dart';
+import 'package:ketertelusuran_mobile/ui/pages/home_page.dart';
+import 'package:ketertelusuran_mobile/ui/pages/sign_up_page.dart';
 import 'package:ketertelusuran_mobile/ui/widgets/buttons.dart';
 import 'package:ketertelusuran_mobile/ui/widgets/forms.dart';
+import 'package:page_transition/page_transition.dart';
 
 class SignInPage extends StatelessWidget {
-  const SignInPage({super.key});
+  const SignInPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController emailController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+
     return Scaffold(
       backgroundColor: whiteBackgroundColor,
       body: ListView(
@@ -50,18 +56,20 @@ class SignInPage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 // NOTE: EMAIL INPUT
-                const CustomFormField(
+                CustomFormField(
                   title: 'Alamat Email',
                   typeFormField: 0,
+                  controller: emailController,
                 ),
                 const SizedBox(
                   height: 16,
                 ),
                 // NOTE: PASSWORD INPUT
-                const CustomFormField(
+                CustomFormField(
                   title: 'Password',
                   obscureText: true,
                   typeFormField: 1,
+                  controller: passwordController,
                 ),
                 const SizedBox(
                   height: 8,
@@ -79,11 +87,25 @@ class SignInPage extends StatelessWidget {
                 CustomFilledButton(
                   title: 'Masuk',
                   onPressed: () {
-                    Navigator.pushNamedAndRemoveUntil(
-                      context,
-                      '/home',
-                      (route) => false,
-                    );
+                    if (emailController.text.isEmpty &&
+                        passwordController.text.isEmpty) {
+                      _showWarningSnackBar(
+                          context, 'Password dan alamat email belum terisi');
+                    } else if (emailController.text.isEmpty) {
+                      _showWarningSnackBar(context, 'Alamat email belum terisi');
+                    } else if (passwordController.text.isEmpty) {
+                      _showWarningSnackBar(context, 'Password belum terisi');
+                    } else {
+                      _showSuccessSnackBar(context);
+                      Navigator.push(
+                        context,
+                        PageTransition(
+                          type: PageTransitionType.fade,
+                          duration: const Duration(milliseconds: 500),
+                          child: HomePage(),
+                        ),
+                      );
+                    }
                   },
                 ),
               ],
@@ -95,11 +117,52 @@ class SignInPage extends StatelessWidget {
           CustomTextButton(
             title: 'Buat Akun Baru',
             onPressed: () {
-              Navigator.pushNamed(context, '/sign-up');
+              Navigator.push(
+                context,
+                PageTransition(
+                  type: PageTransitionType.fade,
+                  duration: const Duration(milliseconds: 500),
+                  child: SignUpPage(),
+                ),
+              );
             },
           ),
         ],
       ),
     );
+  }
+
+  // Fungsi untuk menampilkan notifikasi snack bar saat berhasil login
+  void _showSuccessSnackBar(BuildContext context) {
+    final snackBar = SnackBar(
+      content: Text('Berhasil Masuk!'),
+      duration: Duration(seconds: 2), // Durasi notifikasi
+      backgroundColor: greenColor, // Warna latar belakang notifikasi
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15), // Jari-jari border radius
+      ),
+      behavior: SnackBarBehavior.floating, // Snackbar akan mengambang
+      margin: EdgeInsets.symmetric(
+          vertical: 20, horizontal: 60), // Menerapkan margin
+    );
+    ScaffoldMessenger.of(context)
+        .showSnackBar(snackBar); // Menampilkan notifikasi
+  }
+
+  // Fungsi untuk menampilkan notifikasi snack bar peringatan jika alamat email atau password kosong
+  void _showWarningSnackBar(BuildContext context, String message) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      duration: Duration(seconds: 2), // Durasi notifikasi
+      backgroundColor: Colors.red, // Warna latar belakang notifikasi
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15), // Jari-jari border radius
+      ),
+      behavior: SnackBarBehavior.floating, // Snackbar akan mengambang
+      margin: EdgeInsets.symmetric(
+          vertical: 20, horizontal: 60), // Menerapkan margin
+    );
+    ScaffoldMessenger.of(context)
+        .showSnackBar(snackBar); // Menampilkan notifikasi
   }
 }
